9e1392af58c3ed1399dac967a4678a9a35505e87
ThomasNicolai <thomasnicolai@hotmail.com>
2025-02-13 16:15:47 +0100

    add iframe to article detail page fixes #14

--- app/(dashboard)/articles/[slug]/page.tsx
+++ app/(dashboard)/articles/[slug]/page.tsx
@@ -4,9 +4,19 @@ export default async function Page({
   params: Promise<{ slug: string }>;
 }) {
   const slug = (await params).slug;
+  const content =
+    '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8" /> <title>Hello, world!</title> <meta name="viewport" content="width=device-width,initial-scale=1" /> <meta name="description" content="" /> <link rel="icon" href="favicon.png"> </head> <body> <h1>Hello, world!</h1> </body> </html>';
   const articleData = {
     slug: slug,
-    content: 'Lorem Ipsum'
+    content: content
   };
-  return <div>My Post: {slug}</div>;
+
+  const iframeSrcString =
+    'data:text/html,' + encodeURIComponent(articleData.content);
+  return (
+    <div>
+      My Post: {articleData.slug}
+      <iframe id="ArticleFrame" src={iframeSrcString}></iframe>
+    </div>
+  );
 }
