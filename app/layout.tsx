a8f837ed3a823598ced275a75ab26fcfbabf921a
MohammadIdirs <idrihoms@gmail.com>
2025-03-16 22:49:38 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
+import { connectToDatabase } from '@/lib/db';
 
 export const metadata = {
   title: 'Next.js App Router + NextAuth + Tailwind CSS',
   description:
     'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
 };
-
+connectToDatabase();
 export default function RootLayout({
   children
 }: {
