7433c18d7ffd60a43b22675234760d405c24886a
isakerikstad <isak.erikstad@gmail.com>
2025-04-25 11:33:19 +0200

    fjernet noe med uhell

--- app/(dashboard)/products/page.tsx
+++ app/(dashboard)/products/page.tsx
@@ -119,6 +119,7 @@ export default function ModulesPage() {
       {/* Decorative circle in background */}
       <div className="absolute top-[-65px] left-[-170px] w-[390px] h-[390px] rounded-full transform translate-x-1 translate-y-1/3 bg-gradient-to-b from-[hsl(var(--primary))] to-[hsl(var(--secondary))] p-[55px] pointer-events-none z-0">
         <div className="w-full h-full rounded-full bg-background dark:bg-background"></div>
+      </div>
 
       {/* Header section */}
       <div className="relative z-10 pt-6 px-6">
