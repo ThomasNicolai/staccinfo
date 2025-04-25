9d17e1d25d698be8d848e231d5061355d687beeb
isakerikstad <isak.erikstad@gmail.com>
2025-04-25 11:19:54 +0200

    background changes to all pages to match each other and login

--- app/(dashboard)/suggestions/page.tsx
+++ app/(dashboard)/suggestions/page.tsx
@@ -5,5 +5,20 @@ import { SuggestionsClient } from './suggestion-client';
 
 export default async function SuggestionsPage() {
   const { suggestions, tags } = await getSuggestions();
-  return <SuggestionsClient initialSuggestions={suggestions} allTags={tags} />;
+  
+  return (
+    <div className="relative min-h-screen">
+      {/* Decorative circle in background */}
+      <div className="absolute top-[-65px] left-[-170px] w-[390px] h-[390px] rounded-full transform translate-x-1 translate-y-1/3 bg-gradient-to-b from-[hsl(var(--primary))] to-[hsl(var(--secondary))] p-[55px] pointer-events-none z-0">
+        <div className="w-full h-full rounded-full bg-background dark:bg-background"></div>
+      </div>
+
+      <div className="absolute top-0 right-0 w-[350px] h-[350px] bg-gradient-to-br from-[#7590fa] to-[#bfcafa] rounded-full transform translate-x-1/3 -translate-y-1/3"></div>
+
+      {/* Content */}
+      <div className="relative z-10">
+        <SuggestionsClient initialSuggestions={suggestions} allTags={tags} />
+      </div>
+    </div>
+  );
 }
--- app/(dashboard)/videos/page.tsx
+++ app/(dashboard)/videos/page.tsx
@@ -17,5 +17,19 @@ export default async function VideosPage() {
     })
   );
 
-  return <VideoClient initialVideos={videosWithProgression} />;
+  return (
+    <div className="relative min-h-screen">
+      {/* Decorative circle in background */}
+      <div className="absolute top-[-65px] left-[-170px] w-[390px] h-[390px] rounded-full transform translate-x-1 translate-y-1/3 bg-gradient-to-b from-[hsl(var(--primary))] to-[hsl(var(--secondary))] p-[55px] pointer-events-none z-0">
+        <div className="w-full h-full rounded-full bg-background dark:bg-background"></div>
+      </div>
+
+      <div className="absolute top-0 right-0 w-[350px] h-[350px] bg-gradient-to-br from-[#7590fa] to-[#bfcafa] rounded-full transform translate-x-1/3 -translate-y-1/3"></div>
+
+      {/* Content */}
+      <div className="relative z-10">
+        <VideoClient initialVideos={videosWithProgression} />
+      </div>
+    </div>
+  );
 }
